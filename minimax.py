9877de293073f3f24d5eaf86bd275bbee509c1cd
Anthony Basko <75228079+nunnyu@users.noreply.github.com>
2026-03-18 02:55:42 -0400

    #6: Finished connecting minimax to game state; haven't optimized yet

--- minimax.py
+++ minimax.py
@@ -1,33 +1,80 @@
-class Node:
-    def __init__(self, value=None, children=None):
-        self.value = value
-        self.children = [] if children == None else children
+import chess # TODO: or the board rep we use, hasn't been pushed yet
 
+# class Node:
+#     def __init__(self, value=None, children=None):
+#         self.value = value
+#         self.children = [] if children == None else children
+        
+board = chess.Board()
 
+# TODO: Placeholder evaluation, needs to be way more accurate to real strategy
+def evaluate(board):
+    score = 0
+    for square in chess.SQUARES:
+        piece = board.piece_at(square)
+        if piece:
+            if piece.color == chess.WHITE:
+                score += 1
+            else: 
+                score -= 1
+    
+    return score
 
-def minimax(node, depth, is_maximizing):
-    if depth == 0 or len(node.children) == 0:
-        return node.value
+def minimax(board, depth, is_maximizing):
+    if depth == 0 or board.is_game_over():
+        return evaluate(board)
+    
+    moves = board.legal_moves
     
     if is_maximizing:
         best = float("-inf")
-        for child in node.children:
-            value = minimax(child, depth - 1, False)
+        for move in moves:
+            board.push(move)
+            value = minimax(board, depth - 1, False)          
+            # print(move, value)
+            
+            board.pop() # undo 
             best = max(best, value)
             
         return best
     else: # is minimizing
         best = float("inf")
-        for child in node.children:
-            value = minimax(child, depth - 1, True)
-            best = min(best, value)   
+        for move in moves:
+            board.push(move)
+            value = minimax(board, depth - 1, True)
+            # print(move, value)
+            
+            board.pop() # undo
+            best = min(best, value)
              
         return best
 
-tree = Node(children=[
-    Node(children=[Node(4), Node(5)]),
-    Node(children=[Node(3), Node(7)])
-])
+# Run minimax once for each possible move, and pick the best one 
+def choose_move(board, depth):
+    best_move = None
+    best_value = float("-inf")
+    
+    moves = board.legal_moves
 
-assert minimax(tree, 3, True) == 4
-assert minimax(tree, 3, False) == 5
+    for move in moves:
+        board.push(move)
+        value = minimax(board, depth - 1, False)
+        board.pop()
+
+        if value > best_value:
+            best_value = value
+            best_move = move
+
+    return best_move
+
+# tree = Node(children=[
+#     Node(children=[Node(4), Node(5)]),
+#     Node(children=[Node(3), Node(7)])
+# ])
+
+# assert minimax(tree, 3, True) == 4
+# assert minimax(tree, 3, False) == 5
+
+# minimax(board, 3, True)
+
+print(choose_move(board, 3))
